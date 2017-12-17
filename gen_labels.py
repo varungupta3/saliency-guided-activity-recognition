6a646b742cc0e3c864f6cab722cdbc785ac66d00
Varun <varungp3@gmail.com>
2017-12-16 23:36:31 -0500

    True Saliency Refined for multiple gaze points

--- gen_labels.py
+++ gen_labels.py
@@ -8,8 +8,11 @@ import pickle
 
 labelsDataDir = '../../datasets/GTEA/labels_cleaned/'
 # gazeDataPath = 'Ahmed_American.txt'
-labels_files = glob.glob(labelsDataDir + '*.txt')
+labels_files = glob.glob(labelsDataDir + '*American.txt')
 labels_dict = {}
+
+main_actions_list = []
+objects_master_list = []
 
 for labels_file in labels_files:
     print(labels_file)
@@ -32,12 +35,19 @@ for labels_file in labels_files:
                 frame_range = np.arange(left_frame, right_frame + 1)
                 action2 = actions[1].split('>')[0]
                 list_of_objects = action2.split(',')
+
+                # Append to master list of actions
+                if main_action not in main_actions_list:
+                    main_actions_list.append(main_action)
+                for objects in list_of_objects:
+                    if objects not in objects_master_list:
+                        objects_master_list.append(objects)
+
                 for frame in frame_range:
                     labels_dict[person_recipe][frame] = {}
                     labels_dict[person_recipe][frame]['main_action'] = main_action
                     labels_dict[person_recipe][frame]['other_actions'] = list_of_objects
-
-            # else:
-                # pdb.set_trace()
+                    labels_dict[person_recipe][frame]['main_action_idx'] = [idx+1 for idx in range(len(main_actions_list)) if main_actions_list[idx]==main_action]
+                    labels_dict[person_recipe][frame]['other_action_idx'] = [idx+1 for idx in range(len(objects_master_list)) if objects_master_list[idx]==list_of_objects[0]]
 
 pdb.set_trace()
